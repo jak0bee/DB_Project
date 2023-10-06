2c1ba4ef6c21d280b68dfa1c85f456d632b27b76
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-10-06 19:00:44 +0200

    import events chyba done

--- Code/utils/special_event.py
+++ Code/utils/special_event.py
@@ -1,7 +1,19 @@
 class special_event:
-    def __init__(self, event_type=None, timestamp=None, entity1=None, entity2=None, value=None):
+    def __init__(self, event_type=None, timestamp=None, entity1=None, entity2=None, value=None,
+                 additional_entity_type=None, additional_entity=None):
         self.event_type = event_type
         self.timestamp = timestamp
         self.entity1 = entity1
         self.entity2 = entity2
         self.value = value
+        self.additional_entity_type = additional_entity_type
+        self.additional_entity = additional_entity
+
+    def print(self):
+        print("Event Type:", self.event_type)
+        print("Timestamp:", self.timestamp)
+        print("Entity 1:", self.entity1)
+        print("Entity 2:", self.entity2)
+        print("Value:", self.value)
+        print("Additional Entity Type:", self.additional_entity_type)
+        print("Additional Entity:", self.additional_entity)
