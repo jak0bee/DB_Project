1809a779d5f33b8966b4857def9845494c44d5db
jak0bee <115256446+jak0bee@users.noreply.github.com>
2023-10-10 00:15:45 +0200

    a kto to juz pamieta

--- Code/database_actions/data_import/export_events.py
+++ Code/database_actions/data_import/export_events.py
@@ -1,5 +1,6 @@
 import json
 import re
+import time
 
 from database_actions.data_import.import_events import import_events
 from database_actions.database_manipulation import DatabaseManipulation
@@ -10,15 +11,20 @@ def export_events():
     """
     Exports events to the database
     """
+    start_time = time.time()
     file_name = "../../../Resources/generated_events.txt"
     events = import_events(file_name)
     dbm = DatabaseManipulation()
-    for event in events:
+    le = len(events)
+    for i,event in enumerate(events):
+        print("currently at: ", i,", from ", le)
         try:
             print("checking: " + event.entity1_type, " ", event.entity1['id'])
             print(dbm.check(event.entity1_type, event.entity1['id']))
         except TypeError:
             continue
+    end_time = time.time()
+    print(f"Function executed in {end_time - start_time:.2f} seconds.")
 
 
 export_events()
