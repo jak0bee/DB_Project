29bf31e54f0e29f341c9582dee5393e27ea4f641
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-10-19 19:56:31 +0200

    .

--- Code/chat/front_end/interface.py
+++ Code/chat/front_end/interface.py
@@ -5,10 +5,14 @@ from tkinter import scrolledtext
 def send_message():
     message = message_entry.get()
     if message:
-        chat_text.insert(tk.END, "You:" + '\t', 'you_tag')
-        chat_text.insert(tk.END, f"{message}\n", ('message_tag', 'you_tag'))
+        chat_text.insert(tk.END, "You:\n", 'you_tag')
+        chat_text.insert(tk.END, f"{message}\n\n", 'message_tag')
         message_entry.delete(0, tk.END)
         # Here, you also add code to send the message to the server.
+
+
+def receive_message():
+    pass
 
 
 app = tk.Tk()
@@ -21,7 +25,7 @@ sidebar.pack(side=tk.LEFT, fill=tk.BOTH)
 # Chat area
 chat_text = scrolledtext.ScrolledText(app, wrap=tk.WORD, font=("Arial", 16))
 chat_text.pack(padx=20, pady=20, fill=tk.BOTH, expand=True)
-chat_text.tag_configure('you_tag', foreground='#00FF00')
+chat_text.tag_configure('you_tag', foreground='#00FF00', font=("Arial", 16, 'bold'))
 chat_text.tag_configure('message_tag', lmargin1=50, lmargin2=50)
 chat_text.configure(spacing1=5, spacing3=5)
 
@@ -30,6 +34,5 @@ message_entry = tk.Entry(app, width=50)
 message_entry.pack(padx=20, pady=20, side=tk.LEFT)
 send_button = tk.Button(app, text="Send", command=send_message)
 send_button.pack(padx=20, side=tk.LEFT)
-
 
 app.mainloop()
