1c3a8d1041f0dfbb56b70296c3c03a12a429fe93
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-10-20 23:04:51 +0200

    .

--- Deliverables/week_two/Code/utils/entry_to_dictionary.py
+++ Deliverables/week_two/Code/utils/entry_to_dictionary.py
@@ -1,4 +1,5 @@
-#Marcell Dorkó (6326607)  and Jakub Suszwedyk (6310933)
+# Marcell Dorkó (6326607)  and Jakub Suszwedyk (6310933)
+
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy.model import Model
 
