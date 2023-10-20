106d5017ec74c751697e0c4ca252c0d2de25f372
marcell987 <m.dorko@student.maastrichtuniversity.nl>
2023-10-20 22:52:30 +0200

    .

--- Deliverables/week_one/Code/initial_setup/__init__.py
+++ Deliverables/week_one/Code/initial_setup/__init__.py
@@ -1,4 +1,4 @@
-#Marcell Dorkó (6326607)  and Jakub Suszwedyk (6310933)
+# Marcell Dorkó (6326607)  and Jakub Suszwedyk (6310933)
 table_creation_statements = [
     """CREATE TABLE SpecialEvent
     (
